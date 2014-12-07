b60b5ab87c1c919e52db577b4e7d3900ac5e841e
John Goodman <goodman.jm@gmail.com>
2014-12-07 10:12:49 -0500

    load assets

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,17 @@
 (function() {
+    
+  var gameAssets = {
+    "images": [
+      'assets/images/galaxy_0_72x72.png',
+      'assets/images/galaxy_1_72x72.png',
+      'assets/images/galaxy_2_72x72.png'
+    ]
+  };
   
+  
+  Crafty.init(window.width,window.height,window.document.getElementById('gamescreen'));
+    
+  Crafty.load(gameAssets,function() {
+    Crafty.enterScene('Navigator');
+  });
 })();
